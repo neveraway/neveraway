ecc2d0abf748761b106c5ba966703898ff86cc20
Roy Ashbrook <rashbrook@patriottrans.com>
2021-04-12 14:05:04 -0400

    swap bw for task

--- Program.cs
+++ Program.cs
@@ -1,57 +1,105 @@
 using System;
 using System.Windows.Forms;
 using System.Drawing;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+
 
 namespace neveraway
 {
-    class neverawayApp:Form
+    class neverawayApp : Form
     {
         private NotifyIcon trayIcon;
-        private bool isActive = true;
+        private bool isActive = false;
         private Icon activeIcon = new Icon(SystemIcons.Error, 40, 40);
         private Icon inactiveIcon = new Icon(SystemIcons.Shield, 40, 40);
+        private CancellationTokenSource source;
+        // [DllImport("user32.dll")]
+        // private static extern int SetKeyboardState(byte[] keyState);
 
+        // private const Byte[]
 
         protected override void OnLoad(EventArgs e)
         {
-            Visible       = false; // Hide form window.
+            Visible = false; // Hide form window.
             ShowInTaskbar = false; // Remove from taskbar.
-            Opacity       = 0;     // See through, just in case.
+            Opacity = 0;     // See through, just in case.
             base.OnLoad(e);
         }
 
         [STAThread]
         static void Main()
         {
-             Application.Run(new neverawayApp());
+            Application.Run(new neverawayApp());
         }
 
-        public neverawayApp(){
-            SetupNotifyIcon();
-        }
-        private void SetupNotifyIcon(){
+        public neverawayApp()
+        {
             trayIcon = new System.Windows.Forms.NotifyIcon();
             trayIcon.Icon = activeIcon;
             trayIcon.Visible = true;
-            trayIcon.DoubleClick += (s, e) => OnDoubleClick(s,e);
+            trayIcon.DoubleClick += (s, e) => OnClick(s, e);
             trayIcon.ContextMenuStrip = new System.Windows.Forms.ContextMenuStrip();
-            trayIcon.ContextMenuStrip.Items.Add("Do Something?").Click += (s, e) => OnClick(s,e);
-            trayIcon.ContextMenuStrip.Items.Add($"Exit").Click += (s, e) => OnExit(s,e);
-            trayIcon.ShowBalloonTip(500, "Status", "Started Up!", ToolTipIcon.Info);
+            trayIcon.ContextMenuStrip.Items.Add("Never Away?").Click += (s, e) => OnClick(s, e);
+            trayIcon.ContextMenuStrip.Items.Add($"Exit").Click += (s, e) => OnExit(s, e);
+            ToggleStatus();
+            MaintainStatus();
         }
         private void OnClick(object sender, EventArgs e)
         {
-            MessageBox.Show("You did something!","Congrats!");
-        }
-        private void OnDoubleClick(object sender, EventArgs e)
-        {
-            isActive = !isActive;
-            trayIcon.Icon = isActive ? activeIcon : inactiveIcon;
+            ToggleStatus();
         }
         private void OnExit(object sender, EventArgs e)
         {
+            source?.Cancel();
+            isActive = false;
             trayIcon.Visible = false;
             Application.Exit();
         }
+        private void ToggleStatus()
+        {
+            ((ToolStripMenuItem)trayIcon.ContextMenuStrip.Items[0]).Checked = isActive = !isActive;
+            trayIcon.Icon = isActive ? activeIcon : inactiveIcon;
+            trayIcon.Text = isActive ? "NeverAway is on." : "NeverAway is off.";
+            if (isActive)
+            {
+                MaintainStatus();
+            }
+            else
+            {
+                source?.Cancel();
+            }
+        }
+        public async Task MaintainStatus()
+        {
+            source = new CancellationTokenSource();
+            while (isActive)
+            {
+                Keyboard.KeyUp((byte)Keys.F24);
+                await Task.Delay(10 * 1000);
+                if (source.Token.IsCancellationRequested)
+                {
+                    break;
+                }
+            }
+            source.Dispose();
+        }
+
     }
+    //ref https://stackoverflow.com/questions/16342599/c-sharp-hold-key-in-a-game-application
+    public class Keyboard
+    {
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern void keybd_event(byte bVk, byte bScan, int dwFlags, int dwExtraInfo);
+        const int KEY_UP_EVENT = 0x0002; //Key up flag
+
+        public static void KeyUp(byte key)
+        {
+            keybd_event(key, 0, KEY_UP_EVENT, 0);
+        }
+
+    }
+
 }
